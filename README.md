b21d527792611943a951593715de2b54166746ac
poiuto <poiuto@github.com>
2020-08-29 16:35:09 +0700

    big update =))

--- README.md
+++
@@ -1,3 +0,0 @@
-put ```autostart.sh.destop``` in ```~/.config/autostart/```  
-put folder ```autostart``` in ```~/Documents/```  
-```chmode +x```
--- autostart/autostart.sh
+++
@@ -1,5 +0,0 @@
-# Keyboard
-setxkbmap -option "caps:backspace"
-setxkbmap -option "shift:both_capslock"
-xmodmap $HOME/Documents/autostart/rekeyin
-sleep 1s && xset r rate 233 23
